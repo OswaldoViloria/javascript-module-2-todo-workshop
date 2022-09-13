b8aef35a6191c6f627e0f4ee940bbb4c684bedb7
Oswaldo Viloria <oswaldovil42@gmail.com>
2022-09-13 21:29:02 +0200

    ejercicio 6 en clase hecho

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,33 @@
-alert("hello!");
+let todos = []
+
+const createTodo = (text) => {
+    todos.push(text)
+}
+
+const generateTodoDOM = (todo) => {
+    const todoEl = document.createElement("label")
+    const containerEl = document.createElement("div")
+    const todoText = document.createElement("span")
+    
+    todoText.textContent = todo 
+    containerEl.appendChild(todoText)
+    todoEl.classList.add('list-item')
+    containerEl.classList.add('list-item__container')
+    todoEl.appendChild(containerEl)
+
+    return todoEl
+}
 
 document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    e.preventDefault();
-    const text = e.target.elements.text.value.trim();
-    alert(text);
+    e.preventDefault()
+    const text = e.target.elements.text.value.trim()
+    console.log("texto añadido: " + text)
+
+     if (text.length > 0) {
+        createTodo(text)
+        generateTodoDOM(text)
+        }
+    console.log(todos);
 })
+
+
