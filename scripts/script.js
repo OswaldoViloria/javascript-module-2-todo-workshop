bfb142b8e9e96d46f0596acbd3835550abbf1f8b
Oswaldo Viloria <oswaldovil42@gmail.com>
2022-09-15 20:32:51 +0200

    Ejercicio 10

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,58 @@
 let todos = []
 
-const createTodo = (text) => {
-    todos.push(text)
+const removeTodo = (todoEl) => {
+    const todoIndex = todos.findIndex((todo) => {
+        return todo.toLowerCase() === todoEl.textContent.toLowerCase()
+    })
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+    }
+}
+    
+const renderTodos = (todos) => {
+    const todoList = document.querySelector("#todos")
+    todoList.innerHTML =  " "
+
+    if (todos.length > 0) {
+        todos.forEach((todo) => {todoList.appendChild(generateTodoDOM(todo))})
+    } else {
+        const messageEl = document.createElement('p')
+        messageEl.classList.add('empty-message')
+        messageEl.textContent = 'There are no todos to show'
+        todoList.appendChild(messageEl)
+    }
+
 }
 
 const generateTodoDOM = (todo) => {
-    const todoEl = document.createElement("label")
-    const containerEl = document.createElement("div")
-    const todoText = document.createElement("span")
-    
-    todoText.textContent = todo 
+    const todoEl = document.createElement('label')
+    const containerEl = document.createElement('div')
+    const todoText = document.createElement('span')
+
+    todoText.textContent = todo
     containerEl.appendChild(todoText)
+
     todoEl.classList.add('list-item')
     containerEl.classList.add('list-item__container')
     todoEl.appendChild(containerEl)
 
+    const removeButton = document.createElement('button')
+    removeButton.textContent = 'remove'
+    removeButton.classList.add('button', 'button--text')
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', () => {
+        removeTodo(todoText)
+        renderTodos(todos)
+    })
+
     return todoEl
 }
+
+const createTodo = (text) => {
+    todos.push(text)
+}
+
+
 
 document.querySelector('#new-todo').addEventListener('submit', (e) => {
     e.preventDefault()
@@ -25,7 +61,7 @@ document.querySelector('#new-todo').addEventListener('submit', (e) => {
 
      if (text.length > 0) {
         createTodo(text)
-        generateTodoDOM(text)
+        renderTodos(todos)
         }
     console.log(todos);
 })
