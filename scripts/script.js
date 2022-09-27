dfd2db6a69e144628e56a9fad90e15522858a929
Oswaldo Viloria <oswaldovil42@gmail.com>
2022-09-27 21:35:22 +0200

    Listo Dom 1 y Dom 2

--- scripts/script.js
+++ scripts/script.js
@@ -1,35 +1,45 @@
 let todos = []
 
-const removeTodo = (todoEl) => {
-    const todoIndex = todos.findIndex((todo) => {
-        return todo.toLowerCase() === todoEl.textContent.toLowerCase()
+const filters = {
+    searchTitle: '',
+    showFinished: false,
+    showUnfinished: false
+}
+
+const setFilters = (updates) => {
+    if (typeof updates.searchTitle === 'string') {
+        filters.searchTitle = updates.searchTitle
+    }
+    if (typeof updates.showFinished === 'boolean') {
+        filters.showFinished = updates.showFinished
+    }
+    if (typeof updates.showUnfinished === 'boolean') {
+        filters.showUnfinished = updates.showUnfinished
+    }
+}
+
+const createTodo = (text) => {
+    todos.push({
+        title: text,
+        completed: false
     })
-    if (todoIndex > -1) {
-        todos.splice(todoIndex, 1)
-    }
-}
-    
-const renderTodos = (todos) => {
-    const todoList = document.querySelector("#todos")
-    todoList.innerHTML =  " "
-
-    if (todos.length > 0) {
-        todos.forEach((todo) => {todoList.appendChild(generateTodoDOM(todo))})
-    } else {
-        const messageEl = document.createElement('p')
-        messageEl.classList.add('empty-message')
-        messageEl.textContent = 'There are no todos to show'
-        todoList.appendChild(messageEl)
-    }
-
 }
 
-const generateTodoDOM = (todo) => {
+const generateTodoDOM = (todoObj) => {
     const todoEl = document.createElement('label')
     const containerEl = document.createElement('div')
     const todoText = document.createElement('span')
 
-    todoText.textContent = todo
+    const checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox")
+    checkbox.checked = todoObj.completed
+    containerEl.appendChild(checkbox)
+    checkbox.addEventListener("change",() => {
+        toggleTodo(todoObj.title)
+        renderTodos(todos)
+    })
+
+    todoText.textContent = todoObj.title
     containerEl.appendChild(todoText)
 
     todoEl.classList.add('list-item')
@@ -41,17 +51,56 @@ const generateTodoDOM = (todo) => {
     removeButton.classList.add('button', 'button--text')
     todoEl.appendChild(removeButton)
     removeButton.addEventListener('click', () => {
-        removeTodo(todoText)
+        removeTodo(todoObj.title)
         renderTodos(todos)
     })
 
     return todoEl
 }
 
-const createTodo = (text) => {
-    todos.push(text)
+const renderTodos = (todos) => {
+    // filtered Todos
+    let filteredTodos = todos.filter((todo) => todo.title.toLowerCase().includes(filters.searchTitle.toLowerCase()))
+    if(filters.showFinished && filters.showUnfinished) {
+      // do nothing
+    } else if(filters.showFinished) {
+      filteredTodos = filteredTodos.filter((todo) => todo.completed)
+    } else if(filters.showUnfinished) {
+      filteredTodos = filteredTodos.filter((todo) => !todo.completed)
+    }
+    
+    const todoList = document.querySelector('#todos')
+    todoList.innerHTML = ''
+
+    if (filteredTodos.length > 0) {
+        filteredTodos.forEach((todo) => {
+            todoList.appendChild(generateTodoDOM(todo))
+        })
+    } else {
+        const messageEl = document.createElement('p')
+        messageEl.classList.add('empty-message')
+        messageEl.textContent = 'There are no todos to show'
+        todoList.appendChild(messageEl)
+    }
 }
 
+const toggleTodo = (title) => {
+    const todo = todos.find((todo) => todo.title.toLowerCase() === title.toLowerCase())
+
+    if (todo) {
+        todo.completed = !todo.completed
+    }
+}
+
+const removeTodo = (title) => {
+    const todoIndex = todos.findIndex(
+        (todo) => todo.title.toLowerCase() === title.toLowerCase()
+        );
+
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+    }
+}
 
 
 document.querySelector('#new-todo').addEventListener('submit', (e) => {
@@ -66,4 +115,24 @@ document.querySelector('#new-todo').addEventListener('submit', (e) => {
     console.log(todos);
 })
 
+document.querySelector('#search-text').addEventListener('input', (e) => {
+    setFilters({
+        searchTitle: e.target.value
+    })
+    renderTodos(todos)
+})
+
+document.querySelector('#show-finished').addEventListener('change', (e) => {
+    setFilters({
+        showFinished: e.target.checked
+    })
+    renderTodos(todos)
+})
+
+document.querySelector('#show-unfinished').addEventListener('change', (e) => {
+    setFilters({
+        showUnfinished: e.target.checked
+    })
+    renderTodos(todos)
+})
 
